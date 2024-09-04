ce3dd16279e79cf05e20509434d8ced2d7570c38
MutiaraSianturi <mutiarasianturi006@gmail.com>
2024-09-04 21:26:57 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,16 @@
-// NIM - NAMA
+// 12S24045 - MUTIARA YH SIANTURI
 // NIM - NAMA
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        System.out.println(nama);
+    }
+}
+S
